1e9f3fd730375a4c31d78b386f668736bdc4bf18
Jared <baibinsun@gmail.com>
2022-05-14 02:23:22 +0800

    index page finsh

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,54 @@
 // index.js
-Page({})
+Page({
+  data: {
+    buttonText: '开始模拟',
+    isLoading: false,
+    prodList: [
+      '炸鱿鱼圈',
+      '炸虾',
+      '炸鸡'
+    ],
+    prod: '',
+    isShowPopup: false,
+    scend: 5,
+    timer: null
+  },
+
+  /**
+   * 获取模拟的产品
+   */
+  handleGetProd() {
+    this.setData({
+      buttonText: '匹配产品中...',
+      isLoading: true
+    });
+    this.setData({
+      timer: setTimeout(() => {
+        this.setData({
+          isShowPopup: true,
+          isLoading: false,
+          buttonText: '匹配成功',
+          prod: this.data.prodList[this.handleRandomNum()]
+        });
+      }, 1500)
+    });
+  },
+
+  /**
+   * 进入模拟游戏页面
+   */
+  handleToPlay() {
+    wx.redirectTo({
+      url: '../play/play',
+      complete: (res) => {
+        clearTimeout(this.data.timer);
+      },
+    })
+  },
+  /**
+   * 产生随机数，来获取随机模拟产品
+   */
+  handleRandomNum() {
+    return parseInt(Math.random() * 3);
+  }
+})
